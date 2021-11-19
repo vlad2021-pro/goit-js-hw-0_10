71b9e0937a884ee0382b8ec2a901a04aa02d1676
vlad2021-pro <84739712+vlad2021-pro@users.noreply.github.com>
2021-11-20 00:03:58 +0300

    внесены повторные правки

---
+++ src/fetchCountries.js
@@ -0,0 +1,9 @@
+
+export function fetchCountries(countryName) {
+    return fetch(`https://restcountries.com/v3.1/name/${countryName}`).then(response => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    });
+}
